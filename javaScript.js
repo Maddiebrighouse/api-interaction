a6d52ee2447e1fa433ee7a5ad099ab9ebe0dee51
madeleine-brighouse <31881309+Maddiebrighouse@users.noreply.github.com>
2017-11-17 11:48:41 +1300

    proverbem complete

--- javaScript.js
+++ javaScript.js
@@ -1,30 +1,38 @@
-
 window.onload = function (){
   document.getElementById("ajaxButton").addEventListener('click', makeRequest);
 }
 makeRequest()
 function makeRequest(){
-
   var httpRequest = new XMLHttpRequest();
   if (!httpRequest){
-    alert('Something');
+    alert('Can not create XMLHTTP instace');
     return false;
   }
-httpRequest.onreadystatechange = alertContents;
-httpRequest.open('GET','https://eda-te-reo.herokuapp.com/api/proverbs', true);
-httpRequest.send()
+
+  httpRequest.onreadystatechange = alertContents;
+  httpRequest.open('GET','https://eda-te-reo.herokuapp.com/api/proverbs', true);
+  httpRequest.send()
+
   function alertContents() {
     if (httpRequest.readyState === XMLHttpRequest.DONE){
       if (httpRequest.status === 200) {
-        var quotes = document.getElementById("quotes");
+        //var quotes = document.getElementById("quotes");
+        //quotes.innerHTML = httpRequest.responseText;
+        var quotes = JSON.parse(this.responseText);
+        //showQuotes(quotes)
+        document.getElementById("maori").innerHTML = quotes.source;
+        document.getElementById("english").innerHTML = quotes.translation;
+
         quotes.innerHTML = httpRequest.responseText;
       } else {
         alert('There was a problem with the request.');
       }
     }
   }
+
+
 }
-/*function twitter().click(function(){
-   function (this).attr("href", 'https://twitter.com/intent/tweet?text=' + quotes);
- });
+  /*function twitter().click(function(){
+  function (this).attr("href", 'https://twitter.com/intent/tweet?text=' + quotes);
+});
 */
